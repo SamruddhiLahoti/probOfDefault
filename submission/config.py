0678a58669090e87d77bd191d6f829353ad774e7
SamruddhiLahoti <samruddhi.lahoti@gmail.com>
2023-11-12 16:19:23 -0500

    changed paths

--- submission/config.py
+++ submission/config.py
@@ -1,10 +1,11 @@
 TARGET = "is_default"
 
-DATA_PATH = "/Users/lahosa/Documents/NYU/Fall 2023/ML in Finance/Project/train.csv"
+ROOT_PATH = ""
 
-LR_SAVE_PATH = "/Users/lahosa/Documents/NYU/Fall 2023/ML in Finance/Project/lr_model.pkl"
-NN_SAVE_PATH = "/Users/lahosa/Documents/NYU/Fall 2023/ML in Finance/Project/neural_net"
-XGB_SAVE_PATH = "/Users/lahosa/Documents/NYU/Fall 2023/ML in Finance/Project/xgb_model.json"
+DATA_PATH = ROOT_PATH + "train.csv"
+
+LR_SAVE_PATH = ROOT_PATH + "lr_model.pkl"
+XGB_SAVE_PATH = ROOT_PATH + "xgb_model.json"
 
 NORM_MEAN = 11163910.359835
 NORM_STD = 182824801.73946536
